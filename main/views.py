09d290453b9a835fc2a952567a5c8eadfe928412
MaxKayan <maxim.kayander1@gmail.com>
2020-05-08 16:52:03 +0300

    Some fixes

--- main/views.py
+++ main/views.py
@@ -1,16 +1,18 @@
 import simplejson as simplejson
 from django.shortcuts import render
 
-from main.models import Cartridge
+from main.models import Cartridge, Supply
 
 
 def view(request):
     cartridges = Cartridge.objects.all()
+    supply = Supply.objects.all().order_by("-date")
     data = {***REMOVED***
     for obj in Cartridge.objects.all().values():
         data[obj["id"***REMOVED******REMOVED*** = obj
     cartridges_json = simplejson.dumps(data)
     return render(request, "MainPage.html", {
         "cartridges": cartridges,
-        "cartridges_json": cartridges_json
+        "cartridges_json": cartridges_json,
+        "supply": supply
     ***REMOVED***)
