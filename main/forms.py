7c738d67a71c364295f7bee809abda95916df226
MaxKayan <maxim.kayander1@gmail.com>
2020-05-14 08:15:18 +0300

    Supply form

---
+++ main/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+from main.models import Supply
+
+
+class SupplyForm(ModelForm):
+    class Meta:
+        model = Supply
+        # fields = ['pub_date', 'headline', 'content', 'reporter'***REMOVED***
+        fields = '__all__'
+
