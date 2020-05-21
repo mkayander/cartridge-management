72b2dd49d980c6cc2b7599188070b4bf994c15c9
Crass Air <CrassAir92@gmail.com>
2020-05-21 16:31:52 +0300

    edit settings

--- cartridge/settings.py
+++ cartridge/settings.py
@@ -119,8 +119,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_db',
-        'USER': 'locDja',
-        'PASSWORD': 'qwerty',
+        'USER': 'django',
+        'PASSWORD': 'qwerty654321',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
