85da4bb47d9b4f872fe496f4ddb688fc16eef262
Crass Air <CrassAir92@gmail.com>
2020-05-21 16:53:32 +0300

    edit views, change chat url

--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 
 def room(request):
-    return redirect("http://localhost:3000/")
+    return redirect("http://it-vlshv.dellin.local/")
     # return render(request, 'chat/room.html', {
     #     'room_name': room_name
     # })
