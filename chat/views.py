193984dc35ff9615b9003a7798c6cfc58551eb8a
Crass Air <CrassAir92@gmail.com>
2020-05-21 15:54:11 +0300

    add chat

---
+++ chat/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+
+
+def room(request):
+    return redirect("http://localhost:3000/")
+    # return render(request, 'chat/room.html', {
+    #     'room_name': room_name
+    # })
