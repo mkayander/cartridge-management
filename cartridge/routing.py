1b8d4affa7eb1896b9adcc1b6fe0f5316e4b7be0
Crass Air <CrassAir92@gmail.com>
2020-05-22 11:15:36 +0300

    edit build, change chat in django TEST

--- cartridge/routing.py
+++ cartridge/routing.py
@@ -11,4 +11,3 @@ application = ProtocolTypeRouter({
     ),
 })
 
-print(application)
---
+++ frontend/build/precache-manifest.b56f48a9c9b45b01f5681bb65533edcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57747850a7d191611ab9384044477954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9003ce00536f3e84696",
+    "url": "/static/css/2.4054c8d7.chunk.css"
+  },
+  {
+    "revision": "556a274e404b56893f8a",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c9003ce00536f3e84696",
+    "url": "/static/js/2.82a8e40f.chunk.js"
+  },
+  {
+    "revision": "e7bf5b97138c5ac6aaa306aac1606842",
+    "url": "/static/js/2.82a8e40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556a274e404b56893f8a",
+    "url": "/static/js/main.e7c7a4ec.chunk.js"
+  },
+  {
+    "revision": "50e5276be56b3840ef46",
+    "url": "/static/js/runtime-main.788c5e2c.js"
+  }
+]);
