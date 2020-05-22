182e04da2a3b9d8d7dde8ce3b72407bd4f0b830c
Crass Air <CrassAir92@gmail.com>
2020-05-22 15:28:26 +0300

    add chat history

---
+++ frontend/build/precache-manifest.1618eeb08bc139e0e333aa4d0cbaac4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || [***REMOVED***).concat([
+  {
+    "revision": "34ab2c4f3806e6cd901ee0c7bc6327b7",
+    "url": "/index.html"
+  ***REMOVED***,
+  {
+    "revision": "1d60f61677c318411cde",
+    "url": "/static/css/2.4054c8d7.chunk.css"
+  ***REMOVED***,
+  {
+    "revision": "a664918c2b38b6ff2984",
+    "url": "/static/css/main.69eda509.chunk.css"
+  ***REMOVED***,
+  {
+    "revision": "1d60f61677c318411cde",
+    "url": "/static/js/2.41172f86.chunk.js"
+  ***REMOVED***,
+  {
+    "revision": "e7bf5b97138c5ac6aaa306aac1606842",
+    "url": "/static/js/2.41172f86.chunk.js.LICENSE.txt"
+  ***REMOVED***,
+  {
+    "revision": "a664918c2b38b6ff2984",
+    "url": "/static/js/main.d2c54da0.chunk.js"
+  ***REMOVED***,
+  {
+    "revision": "50e5276be56b3840ef46",
+    "url": "/static/js/runtime-main.788c5e2c.js"
+  ***REMOVED***
+***REMOVED***);
