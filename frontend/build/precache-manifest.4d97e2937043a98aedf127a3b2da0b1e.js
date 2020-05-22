a13c0eff03d26fa5b356032580c1a9fc83259380
Crass Air <CrassAir92@gmail.com>
2020-05-22 15:36:45 +0300

    add chat history

---
+++ frontend/build/precache-manifest.4d97e2937043a98aedf127a3b2da0b1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b80bac1464a6948ed161ab58c9cce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d60f61677c318411cde",
+    "url": "/static/css/2.4054c8d7.chunk.css"
+  },
+  {
+    "revision": "673d00e8981d3aa4ee28",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1d60f61677c318411cde",
+    "url": "/static/js/2.41172f86.chunk.js"
+  },
+  {
+    "revision": "e7bf5b97138c5ac6aaa306aac1606842",
+    "url": "/static/js/2.41172f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673d00e8981d3aa4ee28",
+    "url": "/static/js/main.aa204337.chunk.js"
+  },
+  {
+    "revision": "50e5276be56b3840ef46",
+    "url": "/static/js/runtime-main.788c5e2c.js"
+  }
+]);
