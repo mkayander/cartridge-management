c9ea85ac0957ab8efde10da078ba30e1d69cfda1
Crass Air <CrassAir92@gmail.com>
2020-05-22 16:25:58 +0300

    edit build merge

---
+++ frontend/build/precache-manifest.b7294d5d09f9bcbb16f118da400d5a24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || [***REMOVED***).concat([
+  {
+    "revision": "5e318ae233707de6de4c28108193c6e1",
+    "url": "/index.html"
+  ***REMOVED***,
+  {
+    "revision": "afda7e517be74567f0ba",
+    "url": "/static/css/2.4054c8d7.chunk.css"
+  ***REMOVED***,
+  {
+    "revision": "854bfb41ab3bc1332a4d",
+    "url": "/static/css/main.69eda509.chunk.css"
+  ***REMOVED***,
+  {
+    "revision": "afda7e517be74567f0ba",
+    "url": "/static/js/2.3de83a1e.chunk.js"
+  ***REMOVED***,
+  {
+    "revision": "e7bf5b97138c5ac6aaa306aac1606842",
+    "url": "/static/js/2.3de83a1e.chunk.js.LICENSE.txt"
+  ***REMOVED***,
+  {
+    "revision": "854bfb41ab3bc1332a4d",
+    "url": "/static/js/main.54b8d429.chunk.js"
+  ***REMOVED***,
+  {
+    "revision": "50e5276be56b3840ef46",
+    "url": "/static/js/runtime-main.788c5e2c.js"
+  ***REMOVED***
+***REMOVED***);
