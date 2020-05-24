bb86f639a8a8d303674808f60be28bca78da2011
MaxKayan <maxim.kayander1@gmail.com>
2020-05-24 19:07:47 +0300

    Added Django Debug Toolbar Added localhost to allowed hosts

--- cartridge/urls.py
+++ cartridge/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 ***REMOVED***
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,4 +21,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
 ***REMOVED***
