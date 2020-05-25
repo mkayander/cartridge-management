c0b982b91180169632efb2a3dbc6d7073b6df877
Crass Air <CrassAir92@gmail.com>
2020-05-25 14:17:25 +0300

    change chat react

--- chat/models.py
+++ chat/models.py
@@ -7,3 +7,6 @@ class ChatMessage(models.Model):
     user = models.CharField(max_length=50)
     message = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date']
