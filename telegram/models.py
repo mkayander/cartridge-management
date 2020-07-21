73212da72f2fdf58f0cc0daa439404a7e599341d
Crass Air <CrassAir92@gmail.com>
2020-07-21 12:07:10 +0300

    changed delete method

--- telegram/models.py
+++ telegram/models.py
@@ -5,6 +5,8 @@ class EquipMovement(models.Model):
     # profile = models.ForeignKey(to='telegram.Profile', verbose_name="Profile", on_delete=models.PROTECT)
     message_id = models.PositiveIntegerField(verbose_name="ИД сообщения")
     telegram_user_id = models.PositiveIntegerField(verbose_name="ИД пользователя")
+    bot_answer_message_id = models.PositiveIntegerField(verbose_name="Ответ бота", blank=True)
+    chat_id = models.IntegerField(verbose_name="ИД чата")
     inv_number = models.CharField(max_length=20, verbose_name="Инвентарник")
     comment = models.TextField(verbose_name="Коментарий")
     inv_image = models.ImageField(verbose_name="Фото инвентарника", blank=True)
@@ -19,8 +21,10 @@ class EquipMovement(models.Model):
 
 
 class AdditionalPhoto(models.Model):
-    movement = models.ForeignKey(EquipMovement, verbose_name="EquipMovement", on_delete=models.CASCADE)
+    movement = models.ForeignKey(EquipMovement, related_name='photos', verbose_name="EquipMovement", on_delete=models.CASCADE)
     message_id = models.PositiveIntegerField(verbose_name="ИД сообщения")
+    chat_id = models.IntegerField(verbose_name="ИД чата")
+    # bot_answer_message_id = models.PositiveIntegerField(verbose_name="Ответ бота", blank=True)
     image = models.ImageField(verbose_name="Дополнительное фото", blank=True)
 
     class Meta:
