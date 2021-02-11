3f0c977e2d6f85f2053b963375c2b39eef8e962d
MaxKayan <maxim.kayander1@gmail.com>
2021-02-11 23:14:32 +0300

    New version of channels has changes about the routing

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('chat', consumers.ChatConsumer),
+    path('chat', consumers.ChatConsumer.as_asgi()),
 ]
--- main/routing.py
+++ main/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('liveData', consumers.LiveDataConsumer),
+    path('liveData', consumers.LiveDataConsumer.as_asgi()),
 ]
